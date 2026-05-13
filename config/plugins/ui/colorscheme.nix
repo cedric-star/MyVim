6fc99b449c5d71d3b735069083237baa4c400700
cedric-star <cdrc.wnsch@gmail.com>
2026-05-13 23:07:03 +0200

    color

--- config/plugins/ui/colorscheme.nix
+++ config/plugins/ui/colorscheme.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  opts.termguicolors = false;
+  opts.termguicolors = lib.mkForce false;
   extraPlugins = [ pkgs.vimPlugins.base16-nvim ];
   extraConfigLua = ''
     require('base16-colorscheme').setup('default-dark')
